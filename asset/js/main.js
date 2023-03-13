7bd6cccc9620347309fbdbc23e1b9bf205fb2cb8
Richter96 <castiglione96.rc@gmail.com>
2023-03-13 15:54:57 +0100

    add if in a loop

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,6 +2,8 @@
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 Mail Bonus
 Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
+
+
 Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda:
@@ -21,7 +23,7 @@ Buon divertimento e confermate lettura come al solito :baby-yoda: */
 
 //creiamo un array con alcune mail.
 let dataMail = [
-    "ciao@gmail.com",
+    "ciao",
     "casa@gmail.com",
     "auto@gmail.com",
     "banca@gmail.com",
@@ -29,5 +31,19 @@ let dataMail = [
 ]
 console.log(dataMail)
 
+//chiedo l'indirizzo mail dell'utente
 const userEmail = prompt('inserisci la tua mail')
-console.log(userEmail)
+console.log(userEmail, dataMail)
+
+// creo un loop che conti gli array
+for (let i = 0; i < dataMail.length; i++) {
+// stampo gli array
+// console.log(dataMail[i])
+if (dataMail[i] == userEmail) {
+    console.log('puoi accedere!!!')
+    
+} else if (dataMail[i] != userEmail) {
+    console.log('non puoi accedere')
+}
+}
+
